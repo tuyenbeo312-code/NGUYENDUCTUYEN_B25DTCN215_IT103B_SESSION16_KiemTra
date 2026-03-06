5c66e2b3970f3058429797dcd9a89dafc7b4dfe7
tuyenbeo312-code <tuyenbeo312@gmail.com>
2026-03-06 09:55:35 +0700

    Add files via upload

---
+++ kiemtra/kiemtra.js
@@ -0,0 +1,32 @@
+// Mảng dữ liệu ban đầu
+let tasks = ["Quét nhà", "Giặt quần áo", "Tắm", "Chơi Game", "Đi Ngủ"];
+
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const addBtn = document.querySelector("button");
+
+const renderList = () => {
+  taskList.innerHTML = "";
+
+  tasks.forEach(function (e) {
+    const li = document.createElement("li");
+    li.textContent = e;
+    taskList.appendChild(li);
+  });
+};
+
+function addTask() {
+  let taskName = taskInput.value.trim();
+
+  if (taskName === "") {
+    alert("Vui lòng nhập tên công việc!");
+    return;
+  }
+  tasks.push(taskName);
+  renderList();
+  taskInput.value = "";
+}
+
+addBtn.addEventListener("click", addTask);
+
+renderList();
